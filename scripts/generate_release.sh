f1645192becc5d3f17611341a07ae50d8de517c2
Lucas-Leroy-Henry <lucasleroyhenry@gmail.com>
2024-07-03 10:36:49 -0700

    regenerated exe output, modified generation script

--- scripts/generate_release.sh
+++ scripts/generate_release.sh
@@ -2,3 +2,4 @@
 
 python -m PyInstaller --noconfirm --onedir --windowed --add-data "C:\Users\lucas\Documents\Projects\Code\Contract Work\Grateful Deadle for Brian\work\lib\database\constraints.json;./lib/database/" --add-data "C:\Users\lucas\Documents\Projects\Code\Contract Work\Grateful Deadle for Brian\work\lib\database\db.json;./lib/database/"  "C:\Users\lucas\Documents\Projects\Code\Contract Work\Grateful Deadle for Brian\work\main.py"
 rm -rf build/
+mv dist/main/main.exe dist/main/grateful_grid.exe
