8113a6f31b56181196790640636c8e2dfc1933bf
Lucas-Leroy-Henry <lucasleroyhenry@gmail.com>
2024-08-12 10:28:37 -0700

    trivial changes in generate_release

--- scripts/generate_release.sh
+++ scripts/generate_release.sh
@@ -3,3 +3,4 @@ cwd=$(pwd | sed 's \/c\/ C:\/ g; s \/ \\ g')
 python -m PyInstaller --noconfirm --onedir --windowed --add-data "$cwd\lib\database\constraints.json;./lib/database/" --add-data "$cwd\lib\database\db.json;./lib/database/"  "$cwd\main.py"
 rm -rf build/
 mv dist/main/main.exe dist/main/grateful_grid.exe
+# zip -r dist/grateful_grid.zip dist/main/
